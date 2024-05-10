d624cd87781d0ca1e06f56708dd3b32cc785edc4
S Hari Ram <86203872+SNIPOFIST@users.noreply.github.com>
2024-05-11 02:16:55 +0530

    Update resume_portfolio.py

--- resume_portfolio.py
+++ resume_portfolio.py
@@ -51,7 +51,7 @@ st.write("")
 
 
 st.subheader("Internship - TATA ELXSI", divider="rainbow")
-"During my stint as a Trainee at TATA ELXSI, I dove headfirst into the world of automation and machine learning. From scripting snazzy user interfaces to brewing up predictive ML models, I was on fire. My dedication and knack for tinkering with tech not only earned me kudos but also set the stage for bigger adventures ahead."
+"During my stint as a Trainee at TATA ELXSI, I dove headfirst into the world of automation and machine learning. From scripting snazzy user interfaces to brewing up predictive ML models. My dedication and knack for tinkering with tech not only earned me sppreciations and recognition but also set the stage for bigger adventures ahead."
 st.text("")
 st.write("")
 st.write("")
@@ -59,7 +59,7 @@ st.write("")
 
 
 st.subheader("Associate Software engineer - TATA ELXSI", divider="rainbow")
-"As an Associate Software Engineer at TATA ELXSI, I brought my A-game to the table. From whipping up genius automation frameworks to crafting ML decision models, I was the go-to guy for all things tech. With a smile on my face and a code in my heart, I breezed through challenges, earning nods of approval and high-fives from colleagues."
+"As an Associate Software Engineer at TATA ELXSI, I brought my A-game to the table. From automation frameworks to crafting ML decision models, I was the enthusuaistic and in love with tech. With a smile on my face and a code in my heart, I breezed through challenges, earning nods of approval and high-fives from colleagues, ps; high-fives only when code works and delivered within dealdline :)"
 st.write("")
 st.write("")
 st.write("")
@@ -67,10 +67,12 @@ st.write("")
 
 
 st.subheader("Certifications : https://hariramcertifications.streamlit.app/",divider="rainbow")
+"These are the online course with certifications i achieved to keep my self abreast of techs and to sharpen my knowledge."
 st.write("")
 st.write("")
 st.write("")
 st.subheader("Live Projects: https://list-of-projects.streamlit.app/ ",divider="rainbow")
+"The above link would take you to working projects that I worked on after courses and books I read from online platforms and O'Reilly reading platform. Take a look at it, it should be worht a visit."
 
 
 st.write("")
