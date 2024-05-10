5c4ce62a7c7a3596eb3e4ba02bd8b0870209cee4
S Hari Ram <86203872+SNIPOFIST@users.noreply.github.com>
2024-05-11 01:23:46 +0530

    Update resume_portfolio.py

--- resume_portfolio.py
+++ resume_portfolio.py
@@ -7,8 +7,11 @@ st.set_page_config(page_title="Hari Ram Selvaraj's Portfolio", page_icon="sun", 
 col_1 , col_2, col_3 = st.columns(3)
 with col_1:
     st.write("")
+    st.write("")
+    st.write("")
     st.title("Hari Ram Selvaraj")
     st.caption("Associate Software Engineer | TATA ELXSI ")
+    st.write(")
     st.markdown("PH : +91 89714 56478 | LinkedIn -  [Hari Ram Selvaraj](https://www.linkedin.com/in/hariramselvaraj/)")
     st.write(" 27/A, SBI Bank road, Doddanekundi,")
     st.write(" Marathalli, Bangalore, Karnataka - 560037")
@@ -29,17 +32,21 @@ st.write("")
 st.subheader("About Me",divider="rainbow")
 st.write("")
 st.write("")
-st.text("I'am a 22 year old technology enthusiast, from bangalore, India. Currently working in TATA ELXSI as Associate Software Engineer.")
+st.text("Hey there, I'm Hari Ram Selvaraj, a tech enthusiast with a knack for turning code into something fun. With Python as my trusty sidekick and innovation as my driving force, I'm your go-to guy for all things tech and fun. When I'm not busy coding up a storm, catch me plotting my next chess move or scoring goals on the football field with a grin plastered on my face, so basically i love playing chess anf soccer ")
 st.text("Born and brought up in silicon city of india, gave me an opportunity to witness the industrial and technological revolution of Bangalore.")
 st.text("I Love foot ball, Playing guitar and also to Automate stuffs thats boring to ease the boredom away")
 st.text("buy me a coffee and it would be a good start for a never ending friendship ;)")
 
-st.write("")
-st.write("")
-
-st.write("")
+st.write(""
 st.write("")
 
+st.write("")
+st.write("")
+'''
+Hey there, I'm Hari Ram Selvaraj, your friendly neighborhood tech enthusiast with a knack for turning code into laughter. With Python as my trusty sidekick and innovation as my driving force, I'm your go-to guy for all things tech and fun. When I'm not busy coding up a storm, catch me plotting my next chess move or scoring goals on the football field with a grin plastered on my face.
+My tech journey began at New Horizon College, Bengaluru, where I earned my stripes with a Bachelor of Computer Applications. Cruising through with a stellar CGPA of 9.08 out of 10, I delved deep into Python puzzles and conquered data structures like a boss. Those academic adventures? They laid the groundwork for the wild tech ride ahead.
+During my stint as a Trainee at TATA ELXSI, I dove headfirst into the world of automation and machine learning. From scripting snazzy user interfaces to brewing up predictive ML models, I was on fire. My dedication and knack for tinkering with tech not only earned me kudos but also set the stage for bigger adventures ahead.
+As an Associate Software Engineer at TATA ELXSI, I brought my A-game to the table. From whipping up genius automation frameworks to crafting ML decision models, I was the go-to guy for all things tech. With a smile on my face and a code in my heart, I breezed through challenges, earning nods of approval and high-fives from colleagues.
 
 # Academics 
 
